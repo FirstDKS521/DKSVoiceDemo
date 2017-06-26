b3fb2af9418650c3a2a9c574631762b1a866aaad
duks <duks@robooot.com>
2017-06-26 18:33:59 +0800

    Initial Commit

---
+++ DKSVoiceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKSVoiceDemo
+//
+//  Created by aDu on 2017/6/26.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
